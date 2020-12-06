57e420ae1b37b756518ef5ca67ee184a146213f5
Sebastien <sebastien@flav.es>
2020-12-06 17:15:43 +0100

    chore: remove unused dpd

--- wrapWithProvider.tsx
+++ wrapWithProvider.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import { Global, ThemeProvider } from '@emotion/react';
-import { Provider as ReduxProvider } from 'react-redux';
-
-import store from './src/store';
 
 import reset from './src/styles/reset';
 import theme from './src/styles/theme';
@@ -12,8 +9,8 @@ interface wrapWithProviderProps {
 }
 
 export default ({ element }: wrapWithProviderProps) => (
-  <ReduxProvider store={store}>
+  <>
     <Global styles={reset} />
     <ThemeProvider theme={theme}>{element}</ThemeProvider>
-  </ReduxProvider>
+  </>
 );
